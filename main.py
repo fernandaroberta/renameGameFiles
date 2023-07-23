453de39a09615a50695e4560f0653e5a02795748
Fernanda Roberta <ferorhcp@gmail.com>
2023-07-23 16:19:54 -0300

    Initial version

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import csv
+
+
+dir = "F:\\其他文6\\"
+
+with open("C:\\Backup jogos\\names.txt") as csv_file:
+    files = os.listdir(dir)
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        number = row[0][0:4]
+        file = [i for i in files if i.startswith(number)]
+        for item in file:
+            oldName, ext = os.path.splitext(dir + item)
+            newName = dir + row[0]
+            try:
+                os.rename(oldName + ext, newName + ext)
+            except:
+                print("Erro " + item)
